aacf0b1277ae667812a90931b2fe85915c8b120f
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-06-12 20:27:52 +0530

    Nav bar added

--- components/head.js
+++ components/head.js
@@ -1,26 +1,35 @@
-import React from 'react'
-import NextHead from 'next/head'
-import { string } from 'prop-types'
+import React from "react";
+import NextHead from "next/head";
+import { string } from "prop-types";
 
-const defaultDescription = ''
-const defaultOGURL = ''
-const defaultOGImage = ''
+const defaultDescription = "";
+const defaultOGURL = "";
+const defaultOGImage = "";
 
 const Head = props => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{props.title || ''}</title>
+    <title>{props.title || ""}</title>
     <meta
       name="description"
       content={props.description || defaultDescription}
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link rel="icon" href="../static/img/favicon-16x16-c34ee65b4d10f4a498f372ee6266b1d2.png"  sizes="16x16"></link>
+    <link rel="icon" href="../static/img/favicon-32x32-99b67c3225de8cfd4d55265e2d039d5c.png"  sizes="32x32"></link>
+    <link rel="icon" href="../static/img/favicon-96x96-564e16968542cb2e22e303a7b7371010.png" sizes="96x96"></link>
+    <link
+      href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700,800"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
+      integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg"
+      crossorigin="anonymous"
+    />
     <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta property="og:title" content={props.title || ''} />
+    <meta property="og:title" content={props.title || ""} />
     <meta
       property="og:description"
       content={props.description || defaultDescription}
@@ -31,14 +40,20 @@ const Head = props => (
     <meta property="og:image" content={props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
+
+    <link rel="stylesheet" href="/css/bootstrap-social.css" />
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/@coding-blocks/motley@0.3.43/dist/app.min.css"
+    />
   </NextHead>
-)
+);
 
 Head.propTypes = {
   title: string,
   description: string,
   url: string,
   ogImage: string
-}
+};
 
-export default Head
+export default Head;
--- components/nav.js
+++ components/nav.js
@@ -1,59 +1,152 @@
-import React from 'react'
-import Link from 'next/link'
-
-const links = [
-  { href: 'https://github.com/segmentio/create-next-app', label: 'Github' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+import React from "react";
+import Link from "next/link";
+// import "../static/Css/App.css";
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link prefetch href="/">
-          <a>Home</a>
+  <nav className="main-nav">
+    <div className="nav-left">
+      <span className="hamburger-parent">
+        <i className="hamburger fas fa-bars" />
+      </span>
+      <span className="logo-parent">
+        <Link href="/">
+          <a className="logo-link">
+            <img
+              src="../static/img/dukaan.png"
+              className="nav-logo pointer"
+            />
+          </a>
         </Link>
-      </li>
-      <ul>
-        {links.map(({ key, href, label }) => (
-          <li key={key}>
-            <Link href={href}>
-              <a>{label}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </ul>
+      </span>
+    </div>
 
-    <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
-    `}</style>
+    <div className="dsp-none-sm justify-content-center">
+      <div className="nav-right">
+        <ul className="nav-list">
+
+          <li className="dropdown">
+            <button className="dropbtn dropdown-toggle">
+              COUPONS
+              <i className="fa fa-caret-down" />
+            </button>
+            <div className = "dropdown-content">
+              <div className="flex-row justify-content-center">
+              <Link href="/admin/coupons">
+                <a>All</a>
+                </Link>
+              </div>
+              <div className="divider-h" />
+              <div className="flex-row justify-content-center">
+              <Link href="/admin/coupons/add">
+                <a>Add New</a></Link>
+              </div>
+            </div>
+          </li>
+
+          <li className="dropdown">
+                        <button className="dropbtn dropdown-toggle">PAYMENTS
+                            <i className="fa fa-caret-down"></i>
+                        </button>
+                        <div className="dropdown-content">
+                            <div className="flex-row justify-content-center">
+                            <Link href="/admin/payments">
+                                <a>All</a></Link>
+                            </div>
+                            <div className="divider-h"></div>
+                            <div className="flex-row justify-content-center">
+                            <Link href="/admin/payments/add">
+                                <a>Add New</a></Link>
+                            </div>
+
+                        </div>
+                    </li>
+
+                    <li className="dropdown">
+                        <button className="dropbtn dropdown-toggle">PRODUCTS
+                            <i className="fa fa-caret-down"></i>
+                        </button>
+                        <div className="dropdown-content">
+                            <div className="flex-row justify-content-center">
+                            <Link href="/admin/products">
+                                <a>All</a></Link>
+                            </div>
+                            <div className="divider-h"></div>
+                            <div className="flex-row justify-content-center">
+                            <Link href="/admin/products/add">
+                                <a>Add New</a></Link>
+                            </div>
+
+                        </div>
+                    </li>
+
+                    <li className="dropdown">
+                        <button className="dropbtn dropdown-toggle">Users
+                            <i className="fa fa-caret-down"></i>
+                        </button>
+                        <div className="dropdown-content">
+                            <div className="flex-row justify-content-center">
+                            <Link href="/admin/users">
+                                <a>All</a></Link>
+                            </div>
+                            <div className="divider-h"></div>
+                            <div className="flex-row justify-content-center">
+                            <Link href="/admin/users/add">
+                                <a>Add New</a></Link>
+                            </div>
+
+                        </div>
+                    </li>
+
+          <li className="nav-items pointer capitalize">
+          <Link href="https://account.codingblocks.com">
+            <a className="active">
+              Hi, Tathagat
+            </a></Link>
+          </li>
+          <li className="nav-items pointer">
+            <form method="GET" action="/logout">
+              <div className="button-solid lg">
+                <button type="submit">Logout</button>
+              </div>
+            </form>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  <style jsx>{`
+  .dropdown {
+            overflow: hidden;
+            float: left;
+            margin-left: 40px;
+
+        }
+        .dropdown .dropbtn {
+            border: none;
+            outline: none;
+            font-family: inherit; /* Important for vertical align on mobile phones */
+            margin: 0; /* Important for vertical align on mobile phones */
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+        }
+
+
+        /* Links inside the dropdown */
+        .dropdown-content a {
+            text-align: center;
+            padding: 12px 16px;
+            text-decoration: none;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+      `}</style>
   </nav>
 )
 
-export default Nav
+export default Nav;
