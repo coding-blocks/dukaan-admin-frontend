757e187b793d01dce5c8e8585ad1ab6edb0ac27f
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-06-14 00:40:28 +0530

    dukaan homepage basic layout
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,130 @@
 import React from "react";
 import Link from "next/link";
 import Head from "../components/head";
-import Nav from "../components/nav";
+import Layout from "../components/layout";
 
-const Home = () => (
-  <div>
-    <Head title="Coding Blocks | Dukaan" />
-    <Nav />
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      completeTab: true,
+      incompleteTab: false
+    };
+  }
 
-    <div className="hero">
-  Dukaan Home
-    </div>
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-);
+  toggleTab = () => {
+    this.setState(prevstate => ({
+      completeTab: !prevstate.completeTab,
+      incompleteTab: !prevstate.incompleteTab
+    }));
+  };
+
+  render() {
+    const completeTab = this.state.completeTab;
+    let orders;
+
+    if (completeTab) {
+      orders = <div>Completed Orders.</div>;
+    } else {
+      orders = <div>No Incomplete Orders Found.</div>;
+    }
+
+    return (
+      <div>
+        <Head title="Coding Blocks | Dukaan" />
+        <Layout />
+
+        {/* Search User */}
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-md-12 col-12">
+              <div className="border-card br-20 bg-light-grey mb-5">
+                <h5 className="mb-3">Find User</h5>
+                <div style={{ display: "flex" }}>
+                  <input
+                    name="email"
+                    required
+                    id="email"
+                    type="email"
+                    className="input-text mb-2"
+                    placeholder="Enter email"
+                    onChange={this.handleChange}
+                  />
+                  <button id="search" className="button-solid ml-4 mb-1">
+                    Search
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Form 2  */}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 col-12">
+              <div className="border-card br-20 bg-light-grey mb-5">
+                <h5>User Details</h5>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <p
+                    style={{ margin: "1.6vh", marginLeft: "0" }}
+                    className="red"
+                  >
+                    Username : Tathagat2006
+                  </p>
+
+                  <p style={{ margin: "1.6vh" }}>Name : Tathagat Thapliyal</p>
+
+                  <p style={{ margin: "1.6vh" }}>
+                    Email : tathagat.thapliyal@gmail.com
+                  </p>
+
+                  <p style={{ margin: "1.6vh" }}>Mobile : +91-7503681329</p>
+
+                  <p style={{ margin: "1.6vh" }}>Wallet Amount : ₹ 2320</p>
+
+                  <div>
+                    <button className="button-solid">Make New Payment</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Order history card */}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 col-12">
+              <div className="border-card br-20 bg-light-grey mb-5">
+                <div className="tab-nav-underline mb-5">
+                  <div
+                    className={this.state.completeTab ? "tab active" : "tab"}
+                    onClick={this.toggleTab}
+                  >
+                    Completed Orders
+                  </div>
+                  <div
+                    className={this.state.incompleteTab ? "tab active" : "tab"}
+                    onClick={this.toggleTab}
+                  >
+                    Incomplete Orders
+                  </div>
+                </div>
+                {orders}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
