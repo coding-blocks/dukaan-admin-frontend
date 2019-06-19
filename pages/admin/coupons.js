5636f1157684bd73fd27a305442522f4ccb9b1fd
ananay <i@ananayarora.com>
2019-06-19 23:24:03 +0530

    Added coupon results

--- pages/admin/coupons.js
+++ pages/admin/coupons.js
@@ -9,85 +9,134 @@ class Coupons extends React.Component {
       <div>
         <Head title="Coding Blocks | Dukaan | Coupons" />
         <Layout />
-        <div class="d-flex justify-content-center align-items-center mt-2">
-          <div class="border-card coupon-card">
+        <div class="d-flex mr-5 pr-5">
+          {/* Search Coupons */}
+          <div class="d-flex align-items-center col-md-4 mt-2 ml-5">
+            <div class="border-card coupon-card mt-2">
 
-            {/* Title */}
-            <div class="d-flex justify-content-center mt-2 pb-3">
-              <h2 class="title">Search Coupons</h2>
-            </div>
+              {/* Title */}
+              <div class="d-flex justify-content-center mt-1 pb-3">
+                <h2 class="title">Search Coupons</h2>
+              </div>
 
-            {/* Code */}
-            <FieldWithElement nameCols={3} elementCols={9} name={"Code"}>
-              <input type="text" className="input-text" placeholder="Enter Code" />
-            </FieldWithElement>
+              {/* Code */}
+              <FieldWithElement nameCols={3} elementCols={9} name={"Code"}>
+                <input type="text" className="input-text" placeholder="Enter Code" />
+              </FieldWithElement>
 
-            {/* Categories */}
-            <div>
-              <select id="category" name="category" className="mt-4 col-md-12">
-                <option value="">All Categories</option>
-                <option value="referral">Referral</option>
-                <option value="campus_ambassador">Campus Ambassador</option>
-                <option value="campaign">Campaign</option>
-                <option value="special_discount">Special Discount</option>
-              </select>
-            </div>
-            
-            {/* Products */}
-            <div class="mt-4"  className="col-md-12">
-              <select id="product" name="product">
-                <option value="">All Products</option>
-              </select>
-            </div>
+              {/* Categories */}
+              <div>
+                <select id="category" name="category" className="mt-3 col-md-12">
+                  <option value="">All Categories</option>
+                  <option value="referral">Referral</option>
+                  <option value="campus_ambassador">Campus Ambassador</option>
+                  <option value="campaign">Campaign</option>
+                  <option value="special_discount">Special Discount</option>
+                </select>
+              </div>
+              
+              {/* Products */}
+              <div class="mt-3"  className="col-md-12">
+                <select id="product" name="product">
+                  <option value="">All Products</option>
+                </select>
+              </div>
 
-            {/* Mode */}
-            <div class="mt-4" className="col-md-12">
-              <select id="mode" name="mode">
-                <option value="">All Modes</option>
-                <option value="flat">Flat</option>
-                <option value="percentage">Percentage</option>
-              </select>
-            </div>
+              {/* Mode */}
+              <div class="mt-3" className="col-md-12">
+                <select id="mode" name="mode">
+                  <option value="">All Modes</option>
+                  <option value="flat">Flat</option>
+                  <option value="percentage">Percentage</option>
+                </select>
+              </div>
 
-            {/* Amount */}
-            <FieldWithElement name={"Amount"} nameCols={3} elementCols={9} elementClassName={"pl-4"}>
-              <input type="text" className="input-text" placeholder="Enter Amount" />
-            </FieldWithElement>
+              {/* Amount */}
+              <FieldWithElement name={"Amount"} nameCols={3} elementCols={9} elementClassName={"pl-4"}>
+                <input type="text" className="input-text" placeholder="Enter Amount" />
+              </FieldWithElement>
 
-            {/* Percentage */}
-            <FieldWithElement name={"Percentage"} nameCols={3} elementCols={9} elementClassName={"pl-4"}>
-              <input type="text" className="input-text" placeholder="Enter Percentage" />
-            </FieldWithElement>
+              {/* Percentage */}
+              <FieldWithElement name={"Percentage"} nameCols={3} elementCols={9} elementClassName={"pl-4"}>
+                <input type="text" className="input-text" placeholder="Enter Percentage" />
+              </FieldWithElement>
 
-            {/* Active */}
-            <FieldWithElement name={"Active"} nameCols={3} elementCols={9} elementClassName={"pl-4"}>
-              <select id="active" name="active">
-                <option value="true">True</option>
-                <option value="false">False</option>
-              </select>
-            </FieldWithElement>
+              {/* Active */}
+              <FieldWithElement name={"Active"} nameCols={3} elementCols={9} elementClassName={"pl-4"}>
+                <select id="active" name="active">
+                  <option value="true">True</option>
+                  <option value="false">False</option>
+                </select>
+              </FieldWithElement>
 
-            {/* Show only listed products? */}
-            <FieldWithElement name={"Show only listed products?"} nameCols={6} elementCols={6} elementClassName={"pl-4"}>
-              <select id="listed" name="listed">
-                <option value="no">No</option>
-                <option value="yes">Yes</option>
-              </select>
-            </FieldWithElement>
-            
-            {/* Results per page */}
-            <FieldWithElement name={"Results per page"} nameCols={4} elementCols={8} elementClassName={"pl-4"}>
-              <input type="text" className="input-text" placeholder="Enter Code" value={10} />
-            </FieldWithElement>
-            <div class="d-flex justify-content-center">
-              <button
-                id="search"
-                className="button-solid ml-4 mb-2 mt-4 pl-5 pr-5"
-              >
-                Search
-              </button>
+              {/* Show only listed products? */}
+              <FieldWithElement name={"Show only listed products?"} nameCols={6} elementCols={6} elementClassName={"pl-4"}>
+                <select id="listed" name="listed">
+                  <option value="no">No</option>
+                  <option value="yes">Yes</option>
+                </select>
+              </FieldWithElement>
+              
+              {/* Results per page */}
+              <FieldWithElement name={"Results per page"} nameCols={4} elementCols={8} elementClassName={"pl-4"}>
+                <input type="text" className="input-text" placeholder="Enter Code" value={10} />
+              </FieldWithElement>
+              <div class="d-flex justify-content-center">
+                <button
+                  id="search"
+                  className="button-solid ml-4 mb-2 mt-4 pl-5 pr-5"
+                >
+                  Search
+                </button>
+              </div>
             </div>
           </div>
+          
+          {/* Coupon Results */}
+          <div class="d-flex ml-4 mt-3 col-md-8">
+            <div class="border-card">
+              {/* Title */}
+              <div class="d-flex justify-content-center mt-1 pb-3">
+                <h2 class="title">Coupon Results</h2>
+              </div>
+              {/* Results Table */}
+              <div class="c-overview-leaderboard">
+                <table class="table table-responsive coupons-results-table">
+                  <thead>
+                    <tr>
+                      <th>Code</th>
+                      <th>Category</th>
+                      <th>Referrer Cashback</th>
+                      <th>Mode</th>
+                      <th>Amount</th>
+                      <th>Percentage</th>
+                      <th>Left</th>
+                      <th>Products</th>
+                      <th>Active</th>
+                      <th>Edit</th>
+                      <th>Details</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>ANAOWA</td>
+                      <td>referral</td>
+                      <td>500</td>
+                      <td>flat</td>
+                      <td>500</td>
+                      <td>0</td>
+                      <td>9999</td>
+                      <td>Lol</td>
+                      <td>true</td>
+                      <td><a href="/admin/coupons/edit/1"><button class="button-solid btn btn-default">Edit</button></a></td>
+                      <td><a href="/admin/coupons/1"><button class="button-solid btn btn-default">View</button></a></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     );
