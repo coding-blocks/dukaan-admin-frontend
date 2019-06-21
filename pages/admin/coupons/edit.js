99084f15edcee3732c528ad6e4d98b75379a3f98
ananay <i@ananayarora.com>
2019-06-22 04:48:03 +0530

    Added custom server.js file to handle routes

--- pages/admin/coupons/edit.js
+++ pages/admin/coupons/edit.js
@@ -4,12 +4,23 @@ import Head from '../../../components/head';
 import Layout from "../../../components/Layout";
 
 class EditCoupon extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       couponID: props.router.query.id
     };
   }
+
+  render() {
+    return (
+      <div>
+        <Head title="Coding Blocks | Dukaan | Edit Coupons" />
+        <Layout />
+      </div>
+    )
+  }
+
 }
 
 export default withRouter(EditCoupon);
