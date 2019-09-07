a79e1fdec53cb1973e6a8b2e3a67704f4addd172
tdevm <cr14b17@gmail.com>
2019-09-07 14:18:35 +0530

    add react boundary

--- components/FieldWithElement.js
+++ components/FieldWithElement.js
@@ -2,18 +2,18 @@ import React from 'react';
 import "../styles/components/FieldWithElement.scss";
 
 class FieldWithElement extends React.Component {
-  render() {
-    return (
-        <div className={`mt-3 row d-flex ${ this.props.className }`}>
-          <div className={`col-md-${ this.props.nameCols } name`}>
-            <span className={"text"}>{this.props.name}</span>
-          </div>
-          <div className={`element col-md-${ this.props.elementCols }`}>
-            {this.props.children}
-          </div>
-        </div>
-      )
+    render() {
+        return (
+            <div className={`mt-3 row d-flex ${this.props.className}`}>
+                <div className={`col-md-${this.props.nameCols} name`}>
+                    <span className={"text"}>{this.props.name}</span>
+                </div>
+                <div className={`element col-md-${this.props.elementCols}`}>
+                    {this.props.children}
+                </div>
+            </div>
+        )
     }
-  }
-  
-  export default FieldWithElement;
+}
+
+export default FieldWithElement;
