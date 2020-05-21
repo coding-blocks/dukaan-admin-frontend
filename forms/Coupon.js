0c990151858a9ad885adb9f832946d74ff6dc596
JasmeetLuthra <jasmeetluthr94@gmail.com>
2020-05-21 12:58:45 +0530

    remove previous amount & percentage state on mode change

--- forms/Coupon.js
+++ forms/Coupon.js
@@ -28,6 +28,8 @@ const couponSchema = Yup.object().shape({
         .required('Organization is required'),
     mode: Yup.string()
         .required('Mode is required'),
+    left: Yup.number()
+        .required('Field is required'),
     amount: Yup.number().when('mode', {
         is: (val) => val == "flat",
         then: Yup.number()
@@ -231,7 +233,7 @@ class CouponForm extends React.Component {
                     <Formik initialValues={this.props.data.isEditMode ? this.makeEditCouponContext() : initialValues}
                             validationSchema={couponSchema}
                             onSubmit={this.onSubmit}>
-                        {({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue}) => (
+                        {({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue, setFieldTouched}) => (
 
                             <form onSubmit={handleSubmit}>
                                 <div className={"coupon-card"}>
@@ -247,8 +249,7 @@ class CouponForm extends React.Component {
                                             }}
                                             value={values.organization_id}
                                             className={this.props.data.isEditMode ? "edit-organization" : "organization"}
-                                            disabled={this.props.data.isEditMode}
-                                            required>
+                                            disabled={this.props.data.isEditMode}>
                                             {
                                                 this.props.data.organizations.map((organization) => {
                                                     return (
@@ -273,11 +274,10 @@ class CouponForm extends React.Component {
                                             value={values.code}
                                             onBlur={handleBlur}
                                             onChange={handleChange}
-                                            disabled={this.props.data.isEditMode}
-                                            required/>
+                                            disabled={this.props.data.isEditMode}/>
 
                                         {!this.props.data.isEditMode &&
-                                        <span id="random_coupon" className="red pull-right mt-2"
+                                        <span id="random_coupon" className="red pull-right mt-2 ml-2"
                                               onClick={() => setFieldValue("code", this.setRandomCouponCode())}>
     			                            	Generate Random Code
     			                            </span>
@@ -298,7 +298,6 @@ class CouponForm extends React.Component {
                                             value={values.authority_doc}
                                             onBlur={handleBlur}
                                             onChange={handleChange}
-                                            required
                                         />
                                     </FieldWithElement>
 
@@ -336,7 +335,6 @@ class CouponForm extends React.Component {
                                             onChange={() => setFieldValue("sub_category_id", this.props.handleSubCategoryChange(event, values.category))}
                                             value={values.sub_category_id}
                                             className={this.props.data.isEditMode ? "edit-subcategory" : "subcategory"}
-                                            required
                                             disabled={this.props.data.isEditMode}>
                                             <option value="" key="">Select</option>
                                             {
@@ -389,12 +387,18 @@ class CouponForm extends React.Component {
                                             id="mode"
                                             name="mode"
                                             onBlur={handleBlur}
-                                            onChange={handleChange}
-                                            value={values.mode}
-                                            required
-                                        >
-                                            <option value="flat">Flat</option>
-                                            <option value="percentage">Percentage</option>
+                                            onChange={() => {
+                                                setFieldValue("mode", event.target.value)
+                                                setFieldValue("amount", null)
+                                                setFieldTouched("amount", false)
+                                                setFieldValue("percentage", null)
+                                                setFieldTouched("percentage", false)
+                                                setFieldValue("max_discount", null)
+                                                setFieldTouched("max_discount", false)
+                                             }}
+                                            value={values.mode}>
+                                                <option value="flat">Flat</option>
+                                                <option value="percentage">Percentage</option>
                                         </select>
                                     </FieldWithElement>
 
@@ -456,7 +460,8 @@ class CouponForm extends React.Component {
 
                                     {/* Total number of times a coupon can be used*/}
                                     <FieldWithElement name={"How many times it can be used?*"} nameCols={6}
-                                                      elementCols={6} elementClassName={"pl-4"}>
+                                                      elementCols={6} elementClassName={"pl-4"} errorColor={'tomato'} 
+                                                      errors={touched.left && errors.left}>
                                         <input
                                             type="number"
                                             className={"input-text"}
@@ -466,7 +471,6 @@ class CouponForm extends React.Component {
                                             value={values.left}
                                             min={1}
                                             title="Left can only have numbers"
-                                            required
                                         />
                                     </FieldWithElement>
 
