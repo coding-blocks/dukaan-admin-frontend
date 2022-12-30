930689caa25e70b1cb58bf971876eff3823ef09c
Vibhu Dujari <vdvibhu20@gmail.com>
2022-12-31 03:14:54 +0530

    add proces.env to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,17 +2,17 @@ const path = require('path')
 
 module.exports = {
   env: {
-    API_URL:'https://dukaan.codingblocks.com',
-    TOKEN_URL:'https://dukaan.codingblocks.com/auth/token',
-    DOMAIN:'https://dukaan-admin.codingblocks.com',
-    CALLBACK_URL:'https://dukaan-admin.codingblocks.com/auth',
-    COOKIE_DOMAIN:'.codingblocks.com',
-    CLIENT_ID:'1166947312',
-    SENTRY_DSN:'https://d14fd2af8b56412cac91b44fb7649bf9@sentry.codingblocks.com/39',
-    SENTRY_AUTH_TOKEN:'e385e2c9311d4b198281c82f80503d4a64bb3afa42ac460784f01446eb3db1f6',
-    ONEAUTH_URL:'https://account.codingblocks.com',
-    ONEAUTH_LOGIN_URL:'https://account.codingblocks.com/oauth/authorize?response_type=code&client_id=1166947312&redirect_uri=https://dukaan-admin.codingblocks.com/auth',
-    ONEAUTH_LOGOUT_URL:'https://account.codingblocks.com/logout?redirect=https://dukaan-admin.codingblocks.com/'
+    API_URL: process.env.API_URL,
+    TOKEN_URL: process.env.TOKEN_URL,
+    DOMAIN: process.env.DOMAIN,
+    CALLBACK_URL: process.env.CALLBACK_URL,
+    COOKIE_DOMAIN: process.env.COOKIE_DOMAIN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
+    ONEAUTH_URL: process.env.ONEAUTH_URL,
+    ONEAUTH_LOGIN_URL: process.env.ONEAUTH_LOGIN_URL,
+    ONEAUTH_LOGOUT_URL: process.env.ONEAUTH_LOGOUT_URL
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles/**/*.scss')],
