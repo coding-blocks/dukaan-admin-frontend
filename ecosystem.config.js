ad1faabff1050d826de02bc95dc1b48972498fa9
Vibhu Dujari <vdvibhu20@gmail.com>
2025-09-23 20:32:36 +0530

    update name in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps:[{
-    name: "dukaan-admin-frontend",
+    name: "vmc-dukaan-admin-frontend",
     script: "yarn start -p 5959",
     env_production: {
       NODE_ENV: "production"
