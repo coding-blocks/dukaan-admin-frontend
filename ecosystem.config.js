52d099a47e0b2c33e4c288cafd9a140114a16300
Vibhu Dujari <vdvibhu20@gmail.com>
2025-09-25 14:18:09 +0530

    add env to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,10 @@ module.exports = {
     name: "dukaan-admin-frontend",
     script: "yarn start -p 5959",
     env_production: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      API_URL: "https://vmc-dukaan.codingblocks.com",
+      DOMAIN: "https://vmc-dukaan-admin.codingblocks.com",
+      // COOKIE_DOMAIN: "codingblocks.com"
     }
   }]
 }
